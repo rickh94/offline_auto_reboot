7d93f1bdf690e1e89ecb7921ed2c93f5484b40a4
Rick Henry <fredericmhenry@gmail.com>
2018-05-23 23:24:28 -0400

    fix stupid mistakes, add python3.5 compatibility
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- external.py
+++ external.py
@@ -1,16 +1,16 @@
 import subprocess
 
-from ruamel.yaml import YAML()
+from ruamel.yaml import YAML
 
 
 yaml = YAML()
 with open("/etc/offline_auto_reboot/external_config.yml", 'r') as configfile:
     config = yaml.load(configfile)
 
-host_part = f"{config['username']}@{config['hostname']}"
+host_part = "{u}@{h}".format(u=config['username'], h=config['hostname'])
 try:
     file_path = config['alt_file'] or '/tmp/online'
 except KeyError:
     file_path = '/tmp/online'
 
-subprocess.run(['ssh', '-i', config['ssh_key_path'], host_part, f"touch {file_path}"])
+subprocess.run(['ssh', '-i', config['ssh_key_path'], host_part, "touch {fp}".format(fp=file_path)])
